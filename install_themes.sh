b1b467aae1733590c843a6c429fc4f306a8ff062
Bendney Yu <bendney@gmail.com>
2015-11-22 22:19:33 +0800

    add install theme scripts
     On branch master
     Your branch is ahead of 'origin/master' by 1 commit.
       (use "git push" to publish your local commits)
    
     Changes to be committed:
    	new file:   install_themes.sh

---
+++ install_themes.sh
@@ -0,0 +1,3 @@
+git clone https://github.com/numixproject/numix-icon-theme.git; cp -r numix-icon-theme/Numix /usr/share/icons; cp -r numix-icon-theme/Numix-Light /usr/share/icons
+
+git clone https://github.com/zagortenay333/numix-cinnamon.git; cp -r numix-cinnamon/Numix-Cinnamon /usr/share/themes
