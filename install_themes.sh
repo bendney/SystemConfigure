5195909ecc8023d9c2c084b8bb92e9c5043996e1
Bendney Yu <bendney@gmail.com>
2016-02-18 20:56:27 +0800

    Changed Numix icons to Numix circle icons, added Numix theme

--- install_themes.sh
+++ install_themes.sh
@@ -1,6 +1,9 @@
-git clone https://github.com/numixproject/numix-icon-theme.git;
-sudo cp -r numix-icon-theme/Numix /usr/share/icons;
-sudo cp -r numix-icon-theme/Numix-Light /usr/share/icons;
+git clone https://github.com/numixproject/numix-icon-theme-circle.git;
+sudo cp -r numix-icon-theme-circle/Numix-Circle /usr/share/icons;
+sudo cp -r numix-icon-theme-circle/Numix-Circle-Light /usr/share/icons;
+
+git clone https://github.com/shimmerlproject/Numix.git;
+sudo cp -r Numix /usr/share/themes;
 
 git clone https://github.com/zagortenay333/numix-cinnamon.git;
 sudo cp -r numix-cinnamon/Numix-Cinnamon /usr/share/themes;
